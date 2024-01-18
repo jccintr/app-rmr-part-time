11319c04032c16a94da0eccd0592c1930d283bee
Julio Cesar <jccintr@gmail.com>
2024-01-17 21:01:41 -0300

    sei la

--- Api.js
+++ Api.js
@@ -1,14 +1,14 @@
 // --host=192.168.0.107
 
-const BASE_API = 'http://192.168.0.107:8000/api';
-//const BASE_API = 'https://rmrparttime.com/api';
+//const BASE_API = 'http://192.168.0.107:8000/api';
+const BASE_API = 'https://rmrparttime.com/api';
 
 
 
 export default {
    
-    //base_storage: 'https://rmrparttime.com/storage',
-    base_storage: 'http://192.168.0.107:8000/storage',
+    base_storage: 'https://rmrparttime.com/storage',
+   // base_storage: 'http://192.168.0.107:8000/storage',
     
     
 
